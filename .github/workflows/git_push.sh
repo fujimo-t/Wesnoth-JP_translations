605b52234aeda9a7984320402bef7c082b190b35
fujimo-t <10286050+fujimo-t@users.noreply.github.com>
2025-08-06 05:45:29 +0900

    Fix push script

--- .github/workflows/git_push.sh
+++ .github/workflows/git_push.sh
@@ -2,11 +2,10 @@
 
 git config --global user.name "fujimo-t"
 git config --global user.email "10286050+fujimo-t@users.noreply.github.com"
-git add **/ja.po
 if git diff --exit-code --quiet; then
     echo "No changes to commit."
 else
-    echo "test"
-    git commit -a -m "Pulled from Transifex"
+    git add **/ja.po
+    git commit -m "Pulled from Transifex"
     git push origin main
 fi
