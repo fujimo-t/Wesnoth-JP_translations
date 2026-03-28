cdff284b843372630059f6683de07aa02ff6904b
fujimo-t <10286050+fujimo-t@users.noreply.github.com>
2026-03-29 05:50:48 +0900

    Add Wesnoth 1.20

--- pull_translations.py
+++ pull_translations.py
@@ -3,7 +3,7 @@ import requests
 from transifex.api import transifex_api
 
 organization_name = 'wesnoth-jp'
-project_name = 'wesnoth-118'
+project_names = ['wesnoth-118', 'wesnoth-120']
 language_code = 'ja'
 
 token = os.getenv('TX_TOKEN')
@@ -11,16 +11,17 @@ transifex_api.setup(auth=token)
 
 language = transifex_api.Language.get(code=language_code)
 
-# Download translations foreach resource
-resources = transifex_api.Resource.filter(project=f'o:{organization_name}:p:{project_name}')
-for resource in resources:
-    print('Downloading translations for resource:', resource.slug)
-    
-    out_dir = f'{project_name}/{resource.slug}'
-    os.makedirs(out_dir, exist_ok=True)
+for project_name in project_names:
+    # Download translations foreach resource
+    resources = transifex_api.Resource.filter(project=f'o:{organization_name}:p:{project_name}')
+    for resource in resources:
+        print(f'Downloading translations for {resource.slug} in {project_name}:')
+        
+        out_dir = f'{project_name}/{resource.slug}'
+        os.makedirs(out_dir, exist_ok=True)
 
-    url = transifex_api.ResourceTranslationsAsyncDownload.download(resource=resource, language=language)
-    req = requests.get(url, stream=True)
-    with open(f'{out_dir}/{language_code}.po', 'wb') as fd:
-        for chunk in req.iter_content(chunk_size=10240):
-            fd.write(chunk)
+        url = transifex_api.ResourceTranslationsAsyncDownload.download(resource=resource, language=language)
+        req = requests.get(url, stream=True)
+        with open(f'{out_dir}/{language_code}.po', 'wb') as fd:
+            for chunk in req.iter_content(chunk_size=10240):
+                fd.write(chunk)
